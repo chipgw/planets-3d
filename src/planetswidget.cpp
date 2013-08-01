fde96d52423534e2e59f53774ac6d0a99bf6f8b7
chipgw <gw.chip.gw@gmail.com>
2013-08-01 08:15:06 -0500

    made delay value right with vsync at a different framerate.

--- src/planetswidget.cpp
+++ src/planetswidget.cpp
@@ -74,7 +74,7 @@ void PlanetsWidget::resizeGL(int width, int height) {
 }
 
 void PlanetsWidget::paintGL() {
-    frameTime.restart();
+    delay = frameTime.restart();
 
     if(placingStep == None){
         universe.advance(delay * 20.0f, stepsPerFrame);
@@ -264,11 +264,7 @@ void PlanetsWidget::paintGL() {
         }
     }
 
-    delay = qMax(frameTime.elapsed(), 1);
-
-    timer.start(qMax(0, (1000 / framerate) - delay));
-
-    delay += timer.interval();
+    timer.start(qMax(0, (1000 / framerate) - frameTime.elapsed()));
 
     framecount++;
 
@@ -278,7 +274,7 @@ void PlanetsWidget::paintGL() {
         emit updatePlanetCountStatusMessage(tr("%1 planets").arg(universe.planets.size()));
     }
     emit updateAverageFPSStatusMessage(tr("average fps: %1").arg(framecount / (totalTime.elapsed() * 0.001f)));
-    emit updateFPSStatusMessage(tr("fps: %1").arg(1000.0f / (delay)));
+    emit updateFPSStatusMessage(tr("fps: %1").arg(1000.0f / delay));
 }
 
 void PlanetsWidget::mouseMoveEvent(QMouseEvent* e){
