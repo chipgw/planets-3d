4f0d564030f237e10bbde06eb14aa3fe9a135d6f
chipgw <gw.chip.gw@gmail.com>
2013-10-15 15:35:31 -0500

    add info to QApplication.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,16 @@
 #include "mainwindow.h"
 #include <QApplication>
 #include <QTime>
+#include "version.h"
 
 int main(int argc, char *argv[]){
     QApplication a(argc, argv);
 
     qsrand(QTime::currentTime().msec());
+
+    a.setOrganizationName("chipgw");
+    a.setApplicationName("Planets3D");
+    a.setApplicationVersion(version::getVersionString());
 
     MainWindow w;
     w.showMaximized();
